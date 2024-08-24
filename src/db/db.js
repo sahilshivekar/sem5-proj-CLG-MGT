0fb9997690547243ffb9d91c038335da58d662f7
sahilshivekar <sahilshivekarce@gmail.com>
2024-08-24 21:31:35 +0530

    Fix error of "/" in the db.js file

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const connect = await mongoose.connect(
-            `${process.env.MONGODB_URI}${DB_NAME}`
+            `${process.env.MONGODB_URI}/${DB_NAME}`
         );
         console.log(`\nDATABASE HOST: ${connect.connection.host}`);
         console.log(`DATABASE NAME: ${connect.connection.name}`);
